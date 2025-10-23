d000262477be97bad68d229fd2bf18f59e2fdecf
euguissouza <contato.guisilva4@hotmail.com>
2025-10-23 02:25:27 -0300

    First-commit

---
+++ src/main/java/com/projeto_de_pesquisa/Livraria_System/LivrariaSystemApplication.java
@@ -0,0 +1,14 @@
+package com.projeto_de_pesquisa.Livraria_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaSystemApplication.class, args);
+        System.out.println("\uD83D\uDCA1Servidor Online");
+	}
+
+}
---
+++ src/test/java/com/projeto_de_pesquisa/Livraria_System/LivrariaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_de_pesquisa.Livraria_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
